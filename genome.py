833b0ef90c8b7d3a8f10db605e7c560b5067109e
Alpha Mai <alpha.mai@ryerson.ca>
2019-03-03 18:32:05 -0500

    new link mutation has been added

--- genome.py
+++ genome.py
@@ -1,69 +1,84 @@
 from node import *
 from link import Link
 import math
+from random import choice
 class Genome:
     def __init__(self, num_inputs, num_outputs):
         # create the input and output nodes
-        self.nodes = []
+        self.hidden_nodes = []
         self.input_nodes = []
         for i in range(num_inputs):
-            node = Node(NodeType.INPUT, 0)
-            self.nodes.append(node)
-            self.input_nodes.append(node)
-            print('creating input node: ', i)
+            self.input_nodes.append(Node(NodeType.INPUT, 0))
 
         self.output_nodes = []
         for i in range(num_outputs):
-            node = Node(NodeType.OUTPUT, 0)
-            self.nodes.append(node)
-            self.output_nodes.append(node)
-            print('creating output node:', i)
+            self.output_nodes.append(Node(NodeType.OUTPUT, 0))
 
-        
         # create the links for a simple feedforward neural network
         self.connections = []
         self.innov_counter = 0
-        for i in range(num_inputs):
-            for j in range(num_inputs, num_inputs + num_outputs):
+        for inode in self.input_nodes:
+            for onode in self.output_nodes:
                 weight = random()
-                inode = self.nodes[i]
-                onode = self.nodes[j]
                 connection = Link(inode, onode, weight, self.innov_counter)
                 self.connections.append(connection)
                 inode.add_outgoing(connection)
                 onode.add_incoming(connection)
                 self.innov_counter += 1
-        print(self.connections)
+    
+    def mutate_add_connection(self, innov_number):
+        possible_outputs = self.hidden_nodes + self.output_nodes
+        onode = choice(possible_outputs)
+        possible_inputs = possible_outputs + self.input_nodes
+        inode = choice(possible_inputs)
+
+        # make sure the connection does not exist
+        for link in self.connections:
+            if link.in_node == inode and link.out_node == onode:
+                return False
+        
+        # ensure that both nodes are not output nodes
+        if onode in self.output_nodes and inode in self.output_nodes:
+            return False
+        
+        # Allow recurrent links for now
+        # create new connection
+        weight = random()
+        new_link = Link(inode, onode, weight, innov_number)
+        inode.add_outgoing(new_link)
+        onode.add_incoming(new_link)
+        self.connections.append(new_link)
+        return True
 
     def activate(self, inputs):
         # pass inputs into input nodes
         for idx, inode in enumerate(self.input_nodes):
             inode.active_sum = inputs[idx]
 
+        nodes = self.hidden_nodes + self.output_nodes
         complete = False
         # TODO: maybe a do while loop?
         while not complete:
             complete = True
-            for node in self.nodes:
-                if node.type != NodeType.INPUT:
-                    active_sum = 0
-                    for link in node.incoming:
-                        # the in_node within the link must be active first
-                        if link.enable:
-                            if link.in_node.active_flag or link.in_node.type == NodeType.INPUT:
-                                active_sum += link.weight * link.in_node.active_sum
-                            else:
-                                complete = False
-                                break
-                    if not complete:
-                        continue
-                    if node.type != NodeType.OUTPUT:
-                        active_sum += node.bias
-                    # pass the sum through activation function (sigmoid)
-                    node.active_sum = 1 / (1 + math.exp(-active_sum))
-                    node.active_flag = True
+            for node in nodes:
+                active_sum = 0
+                for link in node.incoming:
+                    # the in_node within the link must be active first
+                    if link.enable:
+                        if link.in_node.active_flag or link.in_node.type == NodeType.INPUT:
+                            active_sum += link.weight * link.in_node.active_sum
+                        else:
+                            complete = False
+                            break
+                if not complete:
+                    continue
+                if node.type != NodeType.OUTPUT:
+                    active_sum += node.bias
+                # pass the sum through activation function (sigmoid)
+                node.active_sum = 1 / (1 + math.exp(-active_sum))
+                node.active_flag = True
 
-        for node in self.nodes:
+        for node in nodes:
             node.active_flag = False
         
         return list(map(lambda node: node.active_sum, self.output_nodes))
